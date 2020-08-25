a30df0f14ca7d68ab967075c0b57f96fedab2603
linapers <linapers@kth.se>
2020-08-25 11:21:46 +0200

    Addition of powertable

--- printtable.py
+++ printtable.py
@@ -7,3 +7,15 @@ def multtable(start, stop, number):
         print(f"{i} x {number} = {i*number}")
 
 multtable(1, 4, 7)
+
+
+def powertable(power, number):
+    """
+    Print a power table for <number> to the power given by the input
+    """
+    for i in range(power, number+1):
+        print(f"{i} ^ {power} = {i**power}")
+
+if __name__ == '__main__':
+    powertable(2,4)
+    powertable(3,3)
