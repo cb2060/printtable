65cc52948de3af4a91820ffa674b7eec2cccb3f5
JPWOE <j.woessmann@web.de>
2020-08-25 09:54:39 +0200

    power

--- printtable.py
+++ printtable.py
@@ -7,3 +7,12 @@ def multtable(start, stop, number):
         print(f"{i} x {number} = {i*number}")
 
 multtable(1, 4, 7)
+
+
+def power(powernumber, number):
+    """Lists all numbers to the power choosen as powernumber
+    """
+    for i in range(1, number+1):
+        print(i**powernumber)
+
+power(2,4)
