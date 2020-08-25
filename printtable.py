b8f480a8300fbc1afcee8a6526aed91bc20aa2be
IlvaNoa <ilvanoa@gmail.com>
2020-08-25 11:06:57 +0200

    Added function powertable

--- printtable.py
+++ printtable.py
@@ -7,3 +7,17 @@ def multtable(start, stop, number):
         print(f"{i} x {number} = {i*number}")
 
 multtable(1, 4, 7)
+
+
+def powertable(power, number):
+    """
+    Print the powertable for <number^power>. 
+    Print this output for every <number^power>.
+    """
+
+    for i in range(1, number+1):
+        print(f"{i} ^{power} = {i**power}")
+
+if __name__ == '__main__':
+    powertable(2, 4)
+    powertable(3, 3)
