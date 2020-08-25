651703051b27a698f1ef51748b967529afbedd01
luiseze <luise.zeckey@gmail.com>
2020-08-25 10:03:23 +0200

    powertable

--- printtable.py
+++ printtable.py
@@ -6,5 +6,14 @@ def multtable(start, stop, number):
     for i in range(start, stop+1):
         print(f"{i} x {number} = {i*number}")
 
+def powertable(power, base):
+    """
+    Print <base>^<power> for base 1 to <base>
+    """
+    for i in range(1, base+1):
+        print(f"{i} ** {power} = {i ** power}")
+
 if __name__ == '__main__':
     multtable(1, 4, 7)
+    powertable(2,4)
+
