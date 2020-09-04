c6a49e532b73ebc1f6bb6b91f82eac96223faaeb
mariaxz <mxandri95@gmail.com>
2020-09-04 12:17:04 +0200

    Modified function

--- printtable.py
+++ printtable.py
@@ -1,9 +1,9 @@
 def multtable(start, stop, number):
     """
-    Print multiplication table for <number>
+    Print power table for <number>
     from <start> to including <stop> 
     """
     for i in range(start, stop+1):
-        print(f"{i} x {number} = {i**number}")
+        print(f"{i} ^ {number} = {i**number}")
 
 multtable(1, 4, 7)
