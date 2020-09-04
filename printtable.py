abfefc522ece52fa18fad62201da58b65ccc7714
mariaxz <mxandri95@gmail.com>
2020-09-04 12:15:30 +0200

    Modified function

--- printtable.py
+++ printtable.py
@@ -4,6 +4,6 @@ def multtable(start, stop, number):
     from <start> to including <stop> 
     """
     for i in range(start, stop+1):
-        print(f"{i} x {number} = {i*number}")
+        print(f"{i} x {number} = {i**number}")
 
 multtable(1, 4, 7)
